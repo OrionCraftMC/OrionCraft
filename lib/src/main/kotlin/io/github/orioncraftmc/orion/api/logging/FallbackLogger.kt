d7f24fec7da6c93190566f47b0ffef1108744911
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-09-09 23:07:25 +0100

    Misc logger refactor

---
+++ lib/src/main/kotlin/io/github/orioncraftmc/orion/api/logging/FallbackLogger.kt
@@ -0,0 +1,27 @@
+package io.github.orioncraftmc.orion.api.logging
+
+object FallbackLogger: Logger {
+	override fun name(): String {
+		return "fallback"
+	}
+
+	override fun debug(str: String, vararg args: Any) {
+		println("[DEBUG] " + str.format(args))
+	}
+
+	override fun info(str: String, vararg args: Any) {
+		println("[INFO] " + str.format(args))
+	}
+
+	override fun warn(str: String, vararg args: Any) {
+		println("[WARN] " + str.format(args))
+	}
+
+	override fun error(str: String, vararg args: Any) {
+		println("[ERROR] " + str.format(args))
+	}
+
+	override fun fatal(str: String, vararg args: Any) {
+		println("[FATAL] " + str.format(args))
+	}
+}
--- lib/src/main/kotlin/io/github/orioncraftmc/orion/api/logging/FallbackLoggerFactory.kt
+++ lib/src/main/kotlin/io/github/orioncraftmc/orion/api/logging/FallbackLoggerFactory.kt
@@ -29,29 +29,3 @@ object FallbackLoggerFactory: LoggerFactory {
 		return FallbackLogger
 	}
 }
-
-object FallbackLogger: Logger {
-	override fun name(): String {
-		return "fallback"
-	}
-
-	override fun debug(str: String, vararg args: Any) {
-		println("[debug] " + str.format(args))
-	}
-
-	override fun info(str: String, vararg args: Any) {
-		println("[info] " + str.format(args))
-	}
-
-	override fun warn(str: String, vararg args: Any) {
-		println("[warn] " + str.format(args))
-	}
-
-	override fun error(str: String, vararg args: Any) {
-		println("[error] " + str.format(args))
-	}
-
-	override fun fatal(str: String, vararg args: Any) {
-		println("[fatal] " + str.format(args))
-	}
-}
