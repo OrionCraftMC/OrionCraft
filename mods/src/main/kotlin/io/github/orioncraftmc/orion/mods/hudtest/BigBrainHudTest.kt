f22a7f521eefeeaaabbe004c69c384eb3bed651f
SD <harrypotter.starwars.shrish@gmail.com>
2021-09-25 00:23:36 +0530

    e

---
+++ mods/src/main/kotlin/io/github/orioncraftmc/orion/mods/hudtest/BigBrainHudTest.kt
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 OrionCraftMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.orioncraftmc.orion.mods.hudtest
+
+import io.github.orioncraftmc.orion.api.bridge.MinecraftBridge
+import io.github.orioncraftmc.orion.api.event.impl.HudRenderEvent
+import io.github.orioncraftmc.orion.api.mod.ModCategory
+import io.github.orioncraftmc.orion.api.mod.OrionMod
+import io.github.orioncraftmc.orion.api.on
+
+object BigBrainHudTest: OrionMod("hudtest", "Hud Test", ModCategory.NEW) {
+	init {
+		on<HudRenderEvent> {
+			MinecraftBridge.fontRenderer.drawString("Orion Hud Render Test", 0, 0, 0xFF0000)
+		}
+	}
+}
